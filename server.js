060788a5fe7d857d163b7534dacf082fc45a61f3
Elias de Souza <elias99-souza@hotmail.com>
2022-11-24 08:50:41 -0300

    atProjetoNodeV1.2

--- server.js
+++ server.js
@@ -13,9 +13,10 @@ const rotas = {
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end(rotas [req.url])
+    res.end(rotas[req.url])
 })
 
 server.listen(port, () => {
-    console.log(`Servidor escutando em http://localhost: ${port}`)
+    console.log('Servidor escutando em http://localhost: ' + port)
+
 })
