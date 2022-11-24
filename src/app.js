44229a7908d23a0fd669f4e074d9ed31931b1b43
Elias de Souza <elias99-souza@hotmail.com>
2022-11-24 10:50:18 -0300

    atProjetoNodeV1.2

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ app.use(express.json())
 
 const livros = [
     { id: 1, 'titulo': 'Senhor dos Aneis' },
-    { id: 1, 'titulo': 'O Hobbit' }
+    { id: 2, 'titulo': 'O Hobbit' }
 ]
 app.get('/', (req, res) => {
     res.status(200).send('Curso de Node')
@@ -15,16 +15,28 @@ app.get('/', (req, res) => {
 app.get('/livros', (req, res) => {
     res.status(200).json(livros)
 })
+app.get('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id)
+    res.json(livros[index])
+})
+
 app.post('/livros', (req, res) => {
     livros.push(req.body)
     res.status(201).send('Livro foi cadastrado com sucesso')
 })
-app.put('/livros/:id', (res, req) => {
-    let index = buscaLivro (req.params.id)
-    livros[index].titulo = req.body.titulo
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id)
+    livros[index].titulo = req.body.titulo //linha para substituir titulo
+    res.json(livros)
+})
+app.delete('/livros/:id', (req, res) => {
+    let {id} = req.params
+    let index = buscaLivro(id)
+    livros.splice(index, 1)
+    res.send('Livro ' + id + ' Removido com sucesso')
 })
 
 function buscaLivro(id) {
-return livros.findIndex(livros => livros.id == id)
+    return livros.findIndex(livro => livro.id == id)
 }
 export default app;
