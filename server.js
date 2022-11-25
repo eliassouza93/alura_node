95504bbc3e7cc5e0a045d5a57cdb9ff5a115be50
Elias de Souza <elias99-souza@hotmail.com>
2022-11-25 09:39:12 -0300

    suindoServerBancoDados

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ import app from "./src/app.js";
 
 const port = process.env.PORT || 3000;
 
-
-
 app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost: ${port}`)
 
--- src/config/dbConect.js
+++ src/config/dbConect.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-mongoose.connect('mongodb+srv://alura:111@alura.gmj8wap.mongodb.net/alura-node');
+mongoose.connect('mongodb+srv://alura:111@alura.gmj8wap.mongodb.net/Node-express');
 
 let db = mongoose.connection;
 
